a68e97334b6e0f4f36cf8fee7f35dcbf998c6a04
Ruben Kuipers <ruben@rubenkuipers.dev>
2021-07-14 09:23:19 +0200

    Revert "add invitation_date"
    This reverts commit 0e0087776b8e2bd77b184d72b8e581452a046aca.

--- src/LaraInvite.php
+++ src/LaraInvite.php
@@ -55,12 +55,6 @@ class LaraInvite implements InvitationInterface
     private $expires;
 
     /**
-     * DateTime of referral invitation date
-     * @var DateTime
-     */
-    private $invitation_date = Carbon::now();
-
-    /**
      * Invitation Model
      * @var Junaidnasir\Larainvite\Models\LaraInviteModel
      */
@@ -224,7 +218,6 @@ class LaraInvite implements InvitationInterface
         $this->instance->entityId   = $this->entityId;
         $this->instance->user_id    = $this->referral;
         $this->instance->valid_till = $this->expires;
-        $this->instance->invitation_date = $this->invitation_date;
         $this->instance->code       = $code;
 
         if (!is_null($beforeSave)) {
